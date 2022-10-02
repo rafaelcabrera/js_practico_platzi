4d6e07b9c71a34cd0a0e137e6f8fd000ed28d0e7
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-01 23:59:55 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-const h1 = document.querySelector("h1");
-const form = document.querySelector("#form");
-const input1 = document.querySelector("#calculo1");
-const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("#btnCalcular");
-const pResult = document.querySelector("#result");
-btn.addEventListener("click", sumarInputValues);
-function sumarInputValues(event) {
-    console.log({event})
-    event.preventDefault();
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+function btnOnClick() {
   const sumaInputs = input1.value + input2.value;
   pResult.innerText = "Resultado: " + sumaInputs;
 }
